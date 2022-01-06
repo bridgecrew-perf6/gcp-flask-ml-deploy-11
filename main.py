5ff359b95120ee6addc86051093df97b90b57f23
Kayphet Inthisone <inthisok@users.noreply.github.com>
2022-01-06 12:03:20 -0600

    update fixes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def hello():
     print("I am inside hello world")
     return "Hello World! CD"
 
-@app.route('echo/<name>')
+@app.route('/echo/<name>')
 def echo(name):
     print(f"This was placed in the url: new-{name}")
     val = {"new-name": name}
